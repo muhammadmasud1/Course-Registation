d48c932c4c986393329e26fc175097169d51915b
muhammadmasud1 <mdmasudrana15020@gmail.com>
2023-12-29 19:12:54 +0600

    card service

--- src/component/main section/Service02.jsx
+++ src/component/main section/Service02.jsx
@@ -1,21 +1,37 @@
+import { useEffect, useState } from "react";
 import styles from "../header/button.module.css"
+import ServiceCard from "./ServiceCard";
+
+ 
+
+
 const Service02 = () => {
+           const [serviceItem, setServiceItem] = useState([])
+          
+            useEffect(() => {
+              fetch("service.json")
+                .then((res) => res.json())
+                .then((data) => setServiceItem(data));
+            }, []);
+
             return (
               <div className={styles.banner}>
                 <div className="text-center py-20">
-                  <h1 className="text-white text-3xl font-semibold">Our Services</h1>
+                  <h1 className="text-white text-3xl font-semibold">
+                    Our Services
+                  </h1>
                   <p className="text-white py-5">
                     We turn information into actionable insights We work to
                     understand your issues <br /> and are driven to ask better
                     questions in the pursuit of making work.
                   </p>
                 </div>
-                <div>
-                        <div>
-                             <div>
-                                    <img src="" alt="" />
-                                    </div>       
-                        </div>
+                <div className="max-w-6xl mx-auto">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-center gap-5">
+                    {serviceItem.map((serviceItem) => (
+                      <ServiceCard key={serviceItem.id} serviceItem={serviceItem}></ServiceCard>
+                    ))}
+                  </div>
                 </div>
               </div>
             );
---
+++ src/component/main section/ServiceCard.jsx
@@ -0,0 +1,28 @@
+import style from './servicecard.module.css'
+
+const ServiceCard = ({serviceItem}) => {
+        const {courseImg,serviceName,description,link} = serviceItem
+         console.log(serviceItem)
+            return (
+              <div className={style.border}>
+                <div className="card shadow-xl border border-white">
+                  <figure className="px-10 pt-10">
+                    <img
+                      src={courseImg}
+                      alt=""
+                      className="rounded-xl"
+                    />
+                  </figure>
+                  <div className="card-body items-center text-center">
+                    <h2 className="card-title text-white">{serviceName}</h2>
+                    <p className='text-white'>{description}</p>
+                    <div className="card-actions text-white py-2">
+                     <a href="#" className='underline' target="_blank" rel="noopener noreferrer">{link}</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+};
+
+export default ServiceCard;
